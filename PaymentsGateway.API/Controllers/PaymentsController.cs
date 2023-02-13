d0a8bd72e2d0377f538f5975d74eb53bdb2c48d1
Kate <aspnetter@gmail.com>
2023-02-14 00:35:18 +0200

    Creating repo...

---
+++ PaymentsGateway.API/Controllers/PaymentsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PaymentsGateway.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PaymentsController : ControllerBase
+{
+
+    private readonly ILogger<PaymentsController> _logger;
+
+    public PaymentsController(ILogger<PaymentsController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "payment")]
+    public IEnumerable<object> Get()
+    {
+        return null;
+    }
+}
+
